77d4d712d6cba0d7bdf48360a6210858662d59dd
nutakoooye <andrkool151@gmail.com>
2021-10-12 11:58:58 +0300

    Created homepage

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Post, Blogger, Comment
 
-# Create your views here.
+
+def index(request):
+
+    num_bloggers = Blogger.objects.count()
+    num_posts = Post.objects.count
+
+    context = {
+        'num_bloggers': num_bloggers,
+        'num_posts': num_posts,
+    }
+    return render(request, 'index.html', context=context)
--- diyblog/urls.py
+++ diyblog/urls.py
@@ -8,5 +8,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-    path('', RedirectView.as_view(url='blog/', permanent=True)),
+    path('', RedirectView.as_view(url='blog/')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
